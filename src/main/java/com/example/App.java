ee888a3f153700296f86be1bc1927bcca88d35ba
anton <a.tashchilin@bk.ru>
2024-04-08 22:43:35 +0400

    delete comments

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -15,17 +15,7 @@ public class App
     public static void main( String[] args ) throws IOException
     {
         var  g = GraphListNode.createGraphFromListNode(new FileInputStream(new File("test")));
-        // System.out.println(g);
-        // // System.out.println(g.getDistanse(new Node(8), new Node(1)));
-        // System.out.println(g.getD());
-        // System.out.println(g.getR());
-        // System.out.println(g.isConnected());
-        // System.out.println(g.isEulerian());
-        // System.out.println(g.isPath(1,6,2,8));
-        // System.out.println(g.getWeight(1, 6));
-        // // g.removeEdge(1, 6);
-        // g.graphListNodeToOut(System.out);
-        // g.graphMatrixAdvecedToOut(System.out);
+        
         System.out.println(g.isFooFullSubgraph());
 
     }
